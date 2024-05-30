800e3e28777e09003ce97de906b97fc4fd1cd450
Klenam <cedeegbe@gmail.com>
2024-05-30 14:41:22 +0000

    Movie Screen UI

--- components/trendingMovies.js
+++ components/trendingMovies.js
@@ -1,10 +1,64 @@
-import { View, Text } from "react-native";
+import { View, Text, Image, Dimensions } from "react-native";
 import React from "react";
+import Carousel from "react-native-reanimated-carousel";
+import { TouchableWithoutFeedback } from "react-native-gesture-handler";
+import { useNavigation } from "@react-navigation/native";
 
-export default function TrendingMovies() {
+const { width, height } = Dimensions.get("window");
+
+export default function TrendingMovies({ data }) {
+  const navigation = useNavigation();
+  const handleClick = () => {
+    navigation.navigate("Movie");
+  };
   return (
-    <View className="mb-8 ">
-      <Text>TrendingMovies</Text>
+    <View style={{ marginBottom: 32 }}>
+      <Text
+        style={{
+          color: "white",
+          fontSize: 20,
+          marginHorizontal: 16,
+          marginBottom: 20,
+        }}
+      >
+        Trending
+      </Text>
+      <Carousel
+        className="ml-10"
+        width={width * 0.8}
+        height={300}
+        data={data}
+        renderItem={({ item }) => (
+          <MovieCard item={item} handleClick={handleClick} />
+        )}
+        mode="parallax-layers"
+        modeConfig={{
+          initialScale: 0.9,
+          scaleRatio: 0.1,
+          translateRatioX: 0.05,
+          translateY: 30,
+          scaleDuration: 600,
+        }}
+        pagingEnabled
+        snapEnabled
+        contentContainerStyle={{ alignItems: "center" }}
+      />
     </View>
   );
 }
+
+const MovieCard = ({ item, handleClick }) => {
+  return (
+    <TouchableWithoutFeedback onPress={handleClick}>
+      <Image
+        className="flex items-center justify-center h-screen"
+        source={require("../assets/moviePoster1.jpg")}
+        style={{
+          width: width * 0.6,
+          height: height * 0.4,
+          borderRadius: 10,
+        }}
+      />
+    </TouchableWithoutFeedback>
+  );
+};
--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -16,16 +16,20 @@ import {
 
 import { styles } from "../theme";
 import TrendingMovies from "../components/trendingMovies";
+import MovieList from "../components/movieList";
 
 const ios = Platform.OS == "ios";
 export default function HomeScreen() {
   const [trending, setTrending] = useState([1, 2, 3]);
+  const [upComing, setUpComing] = useState([1, 2, 3]);
+  const [topRated, setTopRated] = useState([1, 2, 3]);
+
   return (
     <View className="flex-1 bg-neutral-800">
       {/*searchBar and Logo*/}
       <SafeAreaView className={ios ? "-mb-2" : "mb-3"}>
         <StatusBar style="light" />
-        <View className="flex-row justify-between items-center mx-4">
+        <View className="flex-row justify-between items-center mb-5 mx-4">
           <Bars3BottomLeftIcon size="30" strokeWidth={2} color="white" />
           <Text className="text-white text-3xl font-bold">
             <Text style={styles.text}>M</Text>ovies
@@ -39,8 +43,14 @@ export default function HomeScreen() {
         showsVerticalScrollIndicator={false}
         contentContainerStyle={{ paddingBottom: 10 }}
       >
-        {/*Trending Movies carousel*/}
+        {/* Trending Movies */}
         <TrendingMovies data={trending} />
+
+        {/* upcoming movies */}
+        <MovieList title="Upcoming " data={upComing} />
+
+        {/* top rated movies */}
+        <MovieList title="Top Rated " data={topRated} />
       </ScrollView>
     </View>
   );
